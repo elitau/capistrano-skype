9039531381ca972f25efbb3bc49682d30a830de7
Eduard Litau <eduard.litau@gmail.com>
2014-09-17 15:34:52 +0200

    Initial commit of blank gem

---
+++ lib/capistrano/skype.rb
@@ -0,0 +1,7 @@
+require "capistrano/skype/version"
+
+module Capistrano
+  module Skype
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/skype/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Skype
+    VERSION = "0.0.1"
+  end
+end
